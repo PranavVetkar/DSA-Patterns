da5af4d6e58ec7343831d2c1443ab1dedf04abf1
PranavVetkar <pranavv135@gmail.com>
2026-03-10 00:03:31 +0530

    moving zeroes to the end

---
+++ Basics/MoveZeroes.py
@@ -0,0 +1,10 @@
+def MoveZeroes(nums):
+    last_non_zero = 0
+    for i in range(len(nums)):
+        if nums[i] != 0:
+            nums[last_non_zero], nums[i] = nums[i], nums[last_non_zero]
+            last_non_zero += 1
+    return nums
+
+nums = [0, 1, 0, 3, 12]
+print(MoveZeroes(nums))
