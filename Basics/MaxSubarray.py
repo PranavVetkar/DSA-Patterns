1f34f946728892ba7b478a134ec7fbc1b5b882fd
PranavVetkar <pranavv135@gmail.com>
2026-03-09 23:58:34 +0530

    maximum subarray using Kadane's algorithm.

---
+++ Basics/MaxSubarray.py
@@ -0,0 +1,15 @@
+def MaximumSubarray(arr):
+    max_sum = float('-inf')
+    current_sum = 0
+
+    for num in arr:
+        current_sum += num
+        max_sum = max(max_sum, current_sum)
+
+        if current_sum < 0:
+            current_sum = 0
+
+    return max_sum
+
+arr = [-2,1,-3,4,-1,2,1,-5,4]
+print(MaximumSubarray(arr))
