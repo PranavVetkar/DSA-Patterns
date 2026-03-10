d336147e22513c25454e8d745cc21ae800afd8c3
PranavVetkar <pranavv135@gmail.com>
2026-03-10 10:17:59 +0530

    check for sorted array: if every succeeding element > current element

---
+++ Basics/CheckSortedArray.py
@@ -0,0 +1,8 @@
+def CheckSortedArray(arr):
+    for i in range(1, len(arr)):
+        if arr[i] < arr[i - 1]:
+            return False
+    return True
+
+arr = [1, 2, 3, 4, 5]
+print(CheckSortedArray(arr))
