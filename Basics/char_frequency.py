338f94b26db670d01b66eec59a3a230e40a6643f
PranavVetkar <pranavv135@gmail.com>
2026-03-11 00:26:03 +0530

    function to count frequencies of characters in given string

---
+++ Basics/char_frequency.py
@@ -0,0 +1,11 @@
+def char_frequency(s):
+    freq = {}
+    for char in s:
+        if char in freq:
+            freq[char] += 1
+        else:
+            freq[char] = 1
+    return freq
+
+input_string = "hello world"
+print(char_frequency(input_string))
