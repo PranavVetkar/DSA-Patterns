48b54263b78b120b87a5e2b4124ff5357e4eed0d
PranavVetkar <pranavv135@gmail.com>
2026-03-13 23:54:17 +0530

    function to verify if a number is an armstrong number or not

---
+++ Logic-Basics/armstrong_number.py
@@ -0,0 +1,8 @@
+def armstrong_number(num):
+    num_str = str(num)
+    num_digits = len(num_str)
+    armstrong_sum = sum(int(digit) ** num_digits for digit in num_str)
+    return armstrong_sum == num
+
+number = 153
+print(armstrong_number(number))
